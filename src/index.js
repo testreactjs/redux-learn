a23ae06904e1fae3e3c6a106919d0135e662dc5a
Bed Vadim <bedvadim@gmail.com>
2019-02-10 22:54:00 +0600

    2300

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import todoApp from './reducers'
+
+import { addTodo, toggleTodo, setVisibilityFilter, visibilityFilter } from './actions'
+let store = createStore(todoApp)
+
+console.log(store.getState());
+
+let unsubscribe = store.subscribe(()=>
+  console.log(store.getState())
+)
+
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about refucers'))
+store.dispatch(addTodo('Learn about store'))
+
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(visibilityFilter.SHOW_COMPLETED))
+
+unsubscribe();
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
